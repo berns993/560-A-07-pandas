25be33a5de405d209a1306269a799fef584bb554
Berns993 <smbernst@unc.edu>
2025-06-16 18:36:57 -0400

    Final

--- roster.py
+++ roster.py
@@ -10,14 +10,17 @@ for player in roster:
 import pandas as pd
 
 #add Last Name header (changed Dataframe from roster to player)
-player = {"Last Name": ["Claude", "Brown", "Cadaeu"],
-          "First name": ["Ty", "James", "Elliot"],
-          "Height": [79, 82, 74],
-          "Weight": [230, 215, 180]}
+player = {"Last Name": ["Claude", "Brown", "Cadaeu", "Davis", "Tyson", "Davis", "Trimble", "Powell", "Jackson", "Washington"],
+          "First name": ["Ty", "James", "Elliot", "RJ", "Cade", "Elijah", "Seth", "Drake", "Ian", "Jalen"],
+          "Height": [79, 82, 74, 72, 77, 75, 75, 78, 76, 82],
+          "Weight": [230, 215, 180, 180, 200, 215, 195, 195, 190, 235]}
 data = pd.DataFrame(player)
 
 #add bmi column
 data["bmi"]= (data["Weight"]/2.205)/((data["Height"]/39.37)**2)
+
+# Round the bmi column to 2 decimal points
+data["bmi"] = data["bmi"].round(2)
 
 print(data)
 
