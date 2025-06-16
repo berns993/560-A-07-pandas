b60bc81bb2e2963f9ff8e5e3198d6e9aea9f72c8
Berns993 <smbernst@unc.edu>
2025-06-16 18:29:59 -0400

    BMI

--- roster.py
+++ roster.py
@@ -12,7 +12,13 @@ import pandas as pd
 #add Last Name header (changed Dataframe from roster to player)
 player = {"Last Name": ["Claude", "Brown", "Cadaeu"],
           "First name": ["Ty", "James", "Elliot"],
-          "Height": [67, 610, 61],
+          "Height": [79, 82, 74],
           "Weight": [230, 215, 180]}
 data = pd.DataFrame(player)
+
+#add bmi column
+data["bmi"]= (data["Weight"]/2.205)/((data["Height"]/39.37)**2)
+
 print(data)
+
+data.to_csv("bmi.csv")
