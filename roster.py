ea7e55635d061b5dfb6fd7acbcdab936afefcb27
Berns993 <smbernst@unc.edu>
2025-06-16 18:08:53 -0400

    zero header

--- roster.py
+++ roster.py
@@ -1,5 +1,13 @@
 # https://goheels.com/sports/mens-basketball/roster
 
+'''
 roster = [ "Claude", "Brown", "Cadaeu"]
 for player in roster:
     print(player)
+'''
+
+#Import Pandas as pd and create a DataFrame 
+import pandas as pd
+roster = [ "Claude", "Brown", "Cadaeu"]
+data = pd.DataFrame(roster)
+print(data)
